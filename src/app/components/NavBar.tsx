c84025e7582680a849815e5fda12d4b630dfdacc
hashan789 <hashanmalind123@gmail.com>
2025-05-22 12:14:12 +0530

    modified UI

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,10 +1,11 @@
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { ComponentProps, ReactNode } from "react";
 
 
 export default function NavBar({ children } : { children : ReactNode }) {
   return (
-    <div>
+    <div className={cn("w-full h-10 p-5 flex justify-end items-center gap-4 text-lg shadow-black bg-black text-white")}>
       {children}
     </div>
   )
