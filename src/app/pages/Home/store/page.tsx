0728962e167a7f3ed777be41228289c7ad11af99
hashan789 <hashanmalind123@gmail.com>
2025-05-22 12:15:47 +0530

    added card component

--- src/app/pages/Home/store/page.tsx
+++ src/app/pages/Home/store/page.tsx
@@ -1,7 +1,18 @@
+import { formatCurrency } from "@/app/utilities/formatCurrency";
+import storeItems from "../../../data/data.json"
 
+import { CardWithForm } from "@/app/components/CardForm";
 
 export default function Store() {
   return (
-    <div>Store</div>
+    <div className="flex items-center justify-between">
+      {
+        storeItems.map((item) => (
+          <div key={item.id}>
+            <CardWithForm name={item.name} price={formatCurrency(Number(item.price))}/>
+          </div>
+        ))
+      }
+    </div>
   )
 }
